8be9dd98966470ae00310f31e135261b5043814a
vvvar <voinovvladv@gmail.com>
2023-04-20 15:00:29 +0200

    Install Conan from config

--- app/templates/scripts/macos/install.sh
+++ app/templates/scripts/macos/install.sh
@@ -3,4 +3,6 @@ cd $BASEDIR/../../
 
 brew update && brew bundle install --file=config/system/requirements.macos.brew.rb
 
+pip3 install -r config/system/requirements.dev.pip.txt
+
 conan profile detect --force
--- app/templates/scripts/windows/install.bat
+++ app/templates/scripts/windows/install.bat
@@ -4,4 +4,6 @@ cd %ProjectRoot%
 
 call chocolatey install config/system/requirements.windows.choco.config --ignore-package-exit-codes || exit \b 1
 
+call pip3 install -r config/system/requirements.dev.pip.txt
+
 call conan profile detect --force || exit \b 1
